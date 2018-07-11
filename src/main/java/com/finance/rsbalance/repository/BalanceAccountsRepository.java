733d47756b4fdbb76ec2af3e64e2990020642921
RobertoEscobar26 <U20161B003@upc,edu,pe>
2018-07-10 21:12:57 -0500

    the rest api services were implemented, to consult the table of financial debalance accounts.

--- src/main/java/com/finance/rsbalance/repository/BalanceAccountsRepository.java
+++ src/main/java/com/finance/rsbalance/repository/BalanceAccountsRepository.java
@@ -1,7 +1,10 @@
 package com.finance.rsbalance.repository;
 
+import com.finance.rsbalance.model.Accounts;
 import com.finance.rsbalance.model.BalanceAccounts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,16 +15,16 @@ import java.util.Map;
 @Repository
 public interface BalanceAccountsRepository extends JpaRepository<BalanceAccounts, Long> {
 
+
     @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business")
-    public List<Map> findBalanceByPeriodAndBusiness(@Param("period") Long period, @Param("business") Integer business);
+    public List<Map> findBalanceAccountsByPeriodAndBusiness(@Param("period")   Long period,
+                                                            @Param("business") Integer business);
 
-    @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business and id.account_id = :account")
-    public List<Map> findBalanceAccountsByPeriodAndBusinessAndAccount(@Param("period") Long period, @Param("business") Integer business, @Param("account") Integer account);
 
-    @Query("UPDATE BalanceAccounts SET amount = amount WHERE id.monthly_period = :period and id.user_business_id = :business and id.account_id = :account")
-    void updateBalanceAccountsByPeriodAndBusinessAndAccount(@Param("period") Long period,
-                                                            @Param("business") Integer business,
-                                                            @Param("account") Integer account);
-                                                /*            @Param("operation_amount") double operation_amount);    */
+    @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business and ba.id.account_id = :account")
+    public List<Map> findBalanceAccountsByPeriodAndBusinessAndAccount(@Param("period")   Long period,
+                                                                      @Param("business") Integer business,
+                                                                      @Param("account")  Integer account);
+
 
 }
