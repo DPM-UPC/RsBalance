27d20162a04b13328da04c810e816350f4a290a8
RobertoEscobar26 <U20161B003@upc,edu,pe>
2018-07-12 02:15:52 -0500

    Implementacion del desarrollo del api rest, para insertar una nueva cuenta en la entidad balance de cuentas. Implementacion del desarrollo del api rest, para actualizar el monto de una cuenta en la entidad balance de cuentas.

--- src/main/java/com/finance/rsbalance/controller/BalanceAccountsController.java
+++ src/main/java/com/finance/rsbalance/controller/BalanceAccountsController.java
@@ -1,15 +1,13 @@
 package com.finance.rsbalance.controller;
 
-import com.finance.rsbalance.exception.ResourceNotFoundException;
+
 import com.finance.rsbalance.model.BalanceAccounts;
 import com.finance.rsbalance.repository.BalanceAccountsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 
 import javax.validation.Valid;
 import java.util.List;
@@ -22,24 +20,70 @@ public class BalanceAccountsController {
     @Autowired
     BalanceAccountsRepository balanceAccountsRepository;
 
-
-    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public List<Map> getBalanceAccountsByPeriodAndBusiness(@RequestParam(value = "period") Long period,
-                                                           @RequestParam(value = "business") Integer business) {
-
-        List<Map> _result = balanceAccountsRepository.findBalanceAccountsByPeriodAndBusiness(period, business );
-        return _result;
-    }
-
+    BalanceAccounts _balance;
 
     @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public List<Map> getBalanceAccountsByPeriodAndBusinessAndAccount(@RequestParam(value = "period")   Long period,
                                                                      @RequestParam(value = "business") Integer business,
-                                                                     @RequestParam(value = "account")  Integer account){
+                                                                     @RequestParam(value = "account", required = false) Integer account){
 
-        List<Map> _result = balanceAccountsRepository.findBalanceAccountsByPeriodAndBusinessAndAccount(period, business, account);
+        List<Map> _result;
+
+        if(account!=null){
+            _result = balanceAccountsRepository.findBalanceAccountsByPeriodAndBusinessAndAccount(period, business, account);
+        }else{
+            _result = balanceAccountsRepository.findBalanceAccountsByPeriodAndBusiness(period, business);
+        }
         return _result;
     }
 
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public boolean postBalanceAccounts(@Valid @RequestBody BalanceAccounts balanceAccounts) {
+
+        List<Map> _result;
+
+        System.out.println("realizar consulta....");
+
+        _result = balanceAccountsRepository.findBalanceAccountsByPeriodAndBusinessAndAccount(balanceAccounts.getId().getMonthly_period(),
+                                                                                             balanceAccounts.getId().getUser_business_id(),
+                                                                                             balanceAccounts.getId().getAccount_id() );
+
+        if (_result !=null) {
+            /*  update amount of Accounts Balance  */
+            System.out.println("update amount of Accounts Balance");
+            try {
+                balanceAccountsRepository.updateAmountOfBalanceAccountsByPeriodAndBusinessAndAccount(balanceAccounts.getId().getMonthly_period(),
+                                                                                                     balanceAccounts.getId().getUser_business_id(),
+                                                                                                     balanceAccounts.getId().getAccount_id(),
+                                                                                                     balanceAccounts.getAmount());
+
+                return true;
+
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            /*  insert record of Accounts Balance  */
+            System.out.println("Insert record of Accounts Balance");
+            try {
+
+                balanceAccountsRepository.insertBalanceAccounts(balanceAccounts.getId().getMonthly_period(),
+                                                                balanceAccounts.getId().getUser_business_id(),
+                                                                balanceAccounts.getId().getAccount_id(),
+                                                                balanceAccounts.getAmount());
+
+
+                /* balanceAccountsRepository.save(balanceAccounts); */
+
+                return true;
+
+            } catch (Exception e) {
+                return false;
+            }
+
+        }
+
+        }
 
 }
