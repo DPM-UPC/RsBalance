2fec70f5d0459652df009be0a3765e8280d60a24
RobertoEscobar26 <U20161B003@upc,edu,pe>
2018-07-12 02:17:21 -0500

    Implementacion del desarrollo del api rest, para insertar una nueva cuenta en la entidad balance de cuentas. Implementacion del desarrollo del api rest, para actualizar el monto de una cuenta en la entidad balance de cuentas.

--- src/main/java/com/finance/rsbalance/RsbalanceApplication.java
+++ src/main/java/com/finance/rsbalance/RsbalanceApplication.java
@@ -1,10 +1,18 @@
 package com.finance.rsbalance;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class RsbalanceApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RsbalanceApplication.class, args);
