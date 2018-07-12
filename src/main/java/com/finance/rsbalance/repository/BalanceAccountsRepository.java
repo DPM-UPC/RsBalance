4dfde8b265c301bbd37eb5327c37372f16c877ed
RobertoEscobar26 <U20161B003@upc,edu,pe>
2018-07-12 02:16:45 -0500

    Implementacion del desarrollo del api rest, para insertar una nueva cuenta en la entidad balance de cuentas. Implementacion del desarrollo del api rest, para actualizar el monto de una cuenta en la entidad balance de cuentas.

--- src/main/java/com/finance/rsbalance/repository/BalanceAccountsRepository.java
+++ src/main/java/com/finance/rsbalance/repository/BalanceAccountsRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
@@ -16,15 +16,35 @@ import java.util.Map;
 public interface BalanceAccountsRepository extends JpaRepository<BalanceAccounts, Long> {
 
 
-    @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business")
+    @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, ba.amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business")
     public List<Map> findBalanceAccountsByPeriodAndBusiness(@Param("period")   Long period,
                                                             @Param("business") Integer business);
 
 
-    @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business and ba.id.account_id = :account")
+    @Query("SELECT ba.id.monthly_period as period, ba.id.user_business_id as business, ba.id.account_id as account, ba.amount as amount  FROM BalanceAccounts ba WHERE ba.id.monthly_period = :period and ba.id.user_business_id = :business and ba.id.account_id = :account")
     public List<Map> findBalanceAccountsByPeriodAndBusinessAndAccount(@Param("period")   Long period,
                                                                       @Param("business") Integer business,
                                                                       @Param("account")  Integer account);
 
 
-}
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("UPDATE BalanceAccounts ba SET ba.amount =:amount + ba.amount WHERE ba.id.monthly_period =:period AND ba.id.user_business_id =:business AND ba.id.account_id =:account")
+    public void updateAmountOfBalanceAccountsByPeriodAndBusinessAndAccount(@Param("period")   Long period,
+                                                                           @Param("business") Integer business,
+                                                                           @Param("account")  Integer account,
+                                                                           @Param("amount")   double amount);
+
+    @Modifying
+    @Query(value ="INSERT INTO balance_accounts (monthly_period, user_business_id, account_id, amount, creation_date, update_date) VALUES (:period, :business, :account, :amount, now(), now())", nativeQuery = true)
+    public void insertBalanceAccounts(@Param("period")   Long period,
+                                      @Param("business") Integer business,
+                                      @Param("account")  Integer account,
+                                      @Param("amount")   double amount);
+
+
+
+    /*   public void insertBalanceAccounts(period, business, account, amount);   */
+    /*  public BalanceAccounts save(BalanceAccounts balance);  */
+
+    }
+
