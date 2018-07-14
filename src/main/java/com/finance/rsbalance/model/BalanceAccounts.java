7c4a72120f8a43882dcf4480987d673a5b892e7f
RobertoEscobar26 <U20161B003@upc,edu,pe>
2018-07-14 00:30:13 -0500

    The implementation and adjustments in the development of the REST APIs have been completed, to Register, Update, Consult, the Income and Expenses balance accounts.

--- src/main/java/com/finance/rsbalance/model/BalanceAccounts.java
+++ src/main/java/com/finance/rsbalance/model/BalanceAccounts.java
@@ -11,25 +11,34 @@ import java.io.Serializable;
 import java.util.Date;
 
 
+
 @Entity
 @Table(name = "balance_accounts")
-@EntityListeners(AuditingEntityListener.class)
-/*  @JsonIgnoreProperties(value = {"creation_date", "update_date"}, allowGetters = true)  */
-public class BalanceAccounts implements Serializable {
+public class BalanceAccounts  {
+
 
     @EmbeddedId
     private BalanceAccountsId id;
+
+    @Column(name = "amount")
     private Double amount;
 
-    @Column(nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
+    @Column(name = "creation_date")
     private Date creation_date;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
+    @Column(name = "update_date")
     private Date update_date;
+
+
+    public BalanceAccounts(){ }
+
+    public BalanceAccounts(BalanceAccountsId id, Double amount, Date creation_date, Date update_date) {
+        this.id = id;
+        this.amount = amount;
+        this.creation_date = creation_date;
+        this.update_date = update_date;
+    }
+
 
     public BalanceAccountsId getId() {
         return id;
@@ -44,28 +53,24 @@ public class BalanceAccounts implements Serializable {
     }
 
     public void setAmount(Double amount) {
-
         this.amount = amount;
     }
 
     public Date getCreation_date() {
-
         return creation_date;
     }
 
     public void setCreation_date(Date creation_date) {
-
         this.creation_date = creation_date;
     }
 
     public Date getUpdate_date() {
-
         return update_date;
     }
 
     public void setUpdate_date(Date update_date) {
-
         this.update_date = update_date;
     }
+
 
 }
